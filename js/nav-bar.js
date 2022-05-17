54267dd4238722837c6bc8c837d171fc3b5dee4a
mint <snnehir21@gmail.com>
2022-05-17 23:13:27 +0300

    hamburger-menu is added + responsive homepage

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -5,7 +5,12 @@ $(document).ready(()=>{
     $('.nav-link a').each(function () {
         var linkPage = this.href;
         if (activePage == linkPage) {
-            $(this).closest("li").addClass("active");
+            $(this).closest("li").addClass("nav-active");
         }
     });
+    $(".burger").click(function () {
+        $(".burger").toggleClass("active")
+        $(".nav-ul").toggleClass("active")
+        
+    })
 })

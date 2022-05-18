0756043856d0c7fd7ab3a275d61fb1283edd3f10
mint <snnehir21@gmail.com>
2022-05-19 00:38:09 +0300

    some styling işte

--- js/my-translations.js
+++ js/my-translations.js
@@ -3,6 +3,8 @@
     $("#rating_old1").jqxRating({ width: 112, height: 35, value:5 });
     $("#rating_old2").jqxRating({ width: 112, height: 35, value:4 });
     $(".new-translation-container").hide().fadeIn(600)
+    $(".pending-translation-container").hide().fadeIn(600)
+    $("h2").hide().fadeIn(600)
  })
 
  

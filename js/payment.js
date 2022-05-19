a8c42688f000b69913f1ce5f6c444ebac6a9ac94
mint <snnehir21@gmail.com>
2022-05-19 15:27:17 +0300

    more styling bruh

--- js/payment.js
+++ js/payment.js
@@ -1,27 +1,34 @@
 $(document).ready(function () {
+
     var dialog, form,
-   
     dialog = $("#dialog-form").dialog({
         autoOpen: false,
-        height: 500,
+        height: 530,
         width: 350,
         modal: true,
         buttons: {
-            
             "Confirm Payment": function(){
-                // thanks???
-                // redirect
-                alert("Your file has sended!", location.href="../html/my_translation_page.html")
+                let name = $("#name").val()
+                let card_number = parseInt($("#card-number").val())
+                let expiration = $("#expiration-date").val()
+                let cvv = parseInt($("#cvv").val())
                 
-                dialog.dialog("close")
+                if(!$(".price").hasClass("price-active")){
+                    $(".validation-error").html("Please upload your file.")
+                }
+                else if(inputValidation(name, card_number, cvv, expiration)){
+                    alert("Your file has sended! We will inform you when your translation is ready!", location.href="../html/my_translation_page.html")
+                    dialog.dialog("close")
+                }
+                 
             },
-            Cancel: function () {
+            "Cancel": function () {
                 dialog.dialog("close");
+                form[0].reset();
             }
         },
         close: function () {
             form[0].reset();
-            //allFields.removeClass("ui-state-error");
         }
     });
 
@@ -33,3 +40,32 @@ $(document).ready(function () {
         dialog.dialog("open");
     });
 });
+function inputValidation(name, card_number, cvv, expiration_date){
+    if(name.length == 0){
+        $(".validation-error").html("Please enter name on the card.")
+        return false
+    }
+    if(Number.isInteger(card_number)==false || card_number.toString().length != 16){
+        $(".validation-error").html("Please enter a valid card-number.")
+        return false
+    }
+    if(Number.isInteger(cvv) == false || cvv.toString().length != 3){
+        $(".validation-error").html("Please enter a valid cvv.")
+        return false
+    }
+    if(expiration_date.split("/").length != 2){
+        $(".validation-error").html("Please enter a valid expiration date.")
+        return false
+    }
+    if(expiration_date.split("/").length == 2){
+        const date = expiration_date.split("/")
+        const month = parseInt(date[0])
+        const year = parseInt(date[1])
+        if(Number.isInteger(year) == false || Number.isInteger(month) == false || (month > 12 || month < 1)){
+            $(".validation-error").html("Please enter a valid expiration date.")
+            return false
+        }
+        
+    }
+    return true
+}
--- js/translators-profile.js
+++ js/translators-profile.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
-     $("#resume").change(function() {
+    $("#resume").change(function() {
         filename = this.files[0].name;
         $("#selected-file").text(filename)
+        $(".validation-error").html("Please fill all fields.")
         $(".price").toggleClass("price-active")
     });
+    $('.ui-button').each(function(){
+        if($(this).text() === "Confirm Payment"){
+            $(this).toggleClass("confirm-btn")
+        }
+        if($(this).text() === "Cancel"){
+            $(this).toggleClass("cancel-btn")
+        }
+    })
+    
 })

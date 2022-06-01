92eb8deb89a075e7e060211eac635095e7a9f178
mint <snnehir21@gmail.com>
2022-06-01 14:32:09 +0300

    rate dialog

--- js/my-translations.js
+++ js/my-translations.js
@@ -1,15 +1,53 @@
 $(document).ready(function () {
    // each translation must have unique rating and new uploaded translation has 0 star initally
-   $("#rating_new").jqxRating({ width: 112, height: 35, value: 0 });
-   $("#rating_old1").jqxRating({ width: 112, height: 35, value: 5 });
-   $("#rating_old2").jqxRating({ width: 112, height: 35, value: 4 });
+   $("#rating_new").jqxRating({ width: 100, value: 0 });
+   $("#rating_old1").jqxRating({ width: 100, value: 0 });
+   $("#rating_old2").jqxRating({ width: 100, value: 4 });
+   var dialog = $("#dialog-form").dialog({
+      autoOpen: false,
+      height: 300,
+      width: 300,
+      modal: true,
+   })
+   $("#rate-btn-1").click(function () {
+      dialog.dialog("open")
+   })
+   $("#send-btn-1").click(function () {
+      dialog.dialog("close")
+   })
+   // 2nd sample dialog
+   var dialog2 = $("#dialog-form2").dialog({
+      autoOpen: false,
+      height: 300,
+      width: 300,
+      modal: true,
+   })
+   $("#rate-btn-2").click(function () {
+      dialog2.dialog("open")
+   })
+   $("#send-btn-2").click(function () {
+      dialog2.dialog("close")
+   })
+   // 3rd sample dialog
+   var dialog3 = $("#dialog-form3").dialog({
+      autoOpen: false,
+      height: 300,
+      width: 300,
+      modal: true,
+   })
+   $("#rate-btn-3").click(function () {
+      dialog3.dialog("open")
+   })
+   $("#send-btn-3").click(function () {
+      dialog3.dialog("close")
+   })
    $("footer").hide()
    const child_num = document.getElementById("translations").children.length
    $("#translations").children().each(function (index) {
       if (index < child_num)
-        $(this).hide().delay(300 * index).fadeIn(300);
+         $(this).hide().delay(300 * index).fadeIn(300);
    });
-   async function show_after(){
+   async function show_after() {
       await sleep(1000) // wait a second for content
       $("footer").show()
    }
